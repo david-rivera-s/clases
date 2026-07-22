6a9315193031a70d662a1104fb6c2c7f5a4ec943
DAVID ALEJANDRO RIVERA SALVADOR <da.rivera@movilis.edu.ec>
2026-07-22 18:37:44 -0500

    feat(mensajeria): agregar clases Mensajeria y TestMensajeria con metodos sin retorno

---
+++ src/com/krakedev/Mensajeria.java
@@ -0,0 +1,17 @@
+package com.krakedev;
+
+public class Mensajeria {
+	
+	public void saludar() {
+		
+		System.out.println("Hola");
+		
+	}
+	
+	public void saludarPersona(String nombre, String apodo) {
+		
+		System.out.println("Hola: "+nombre+" Tu apodo es: "+ apodo);
+
+	}
+
+}
---
+++ src/com/krakedev/TestMensajeria.java
@@ -0,0 +1,16 @@
+package com.krakedev;
+
+public class TestMensajeria {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Mensajeria m1 = new Mensajeria();
+		
+		m1.saludar();
+		
+		m1.saludarPersona("Messi", "La Pulga");
+
+	}
+
+}
