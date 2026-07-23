3e7a5f7b02d19b3b7d2cd815f8abf697b3b6a077
DAVID ALEJANDRO RIVERA SALVADOR <da.rivera@movilis.edu.ec>
2026-07-23 12:59:59 -0500

    refactor(clases): 03. Generacion Automatica de Getters y Setters

--- src/com/krakedev/Fecha.java
+++ src/com/krakedev/Fecha.java
@@ -6,27 +6,25 @@ public class Fecha {
 	private int mes;
 	private int anio;
 	
-	public int  getDia() {
+	public int getDia() {
 		return dia;
+	}
+	public void setDia(int dia) {
+		this.dia = dia;
 	}
 	
 	public int getMes() {
 		return mes;
 	}
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
 	
 	public int getAnio() {
 		return anio;
 	}
-	
-	public void setDia(int nuevoDia) {
-		dia = nuevoDia;
+	public void setAnio(int anio) {
+		this.anio = anio;
 	}
 	
-	public void setMes(int nuevoMes) {
-		mes = nuevoMes;
-	}
-	
-	public void setAnio (int nuevoAnio) {
-		anio = nuevoAnio;
-	}
 }
