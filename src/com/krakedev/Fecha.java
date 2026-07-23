534e8ef9c3ddb12e1f3bc4bef4e008109990996f
DAVID ALEJANDRO RIVERA SALVADOR <da.rivera@movilis.edu.ec>
2026-07-23 11:56:56 -0500

    feat(clases): implementar getters y setters en la clase Fecha

---
+++ src/com/krakedev/Fecha.java
@@ -0,0 +1,32 @@
+package com.krakedev;
+
+public class Fecha {
+	
+	private int dia;
+	private int mes;
+	private int anio;
+	
+	public int  getDia() {
+		return dia;
+	}
+	
+	public int getMes() {
+		return mes;
+	}
+	
+	public int getAnio() {
+		return anio;
+	}
+	
+	public void setDia(int nuevoDia) {
+		dia = nuevoDia;
+	}
+	
+	public void setMes(int nuevoMes) {
+		mes = nuevoMes;
+	}
+	
+	public void setAnio (int nuevoAnio) {
+		anio = nuevoAnio;
+	}
+}
