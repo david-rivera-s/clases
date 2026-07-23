38fc625dc935caf7f52ec8db0f52035f0dd786f5
DAVID ALEJANDRO RIVERA SALVADOR <da.rivera@movilis.edu.ec>
2026-07-23 13:15:08 -0500

    refactor(vehiculos): actualizar Vehiculos y  VehiculoTest para usar getters y setters

--- src/com/krakedev/Vehiculo.java
+++ src/com/krakedev/Vehiculo.java
@@ -2,9 +2,37 @@ package com.krakedev;
 
 public class Vehiculo {
 	
-	public String marca;
-	public String modelo;
-	public String anio;
-	public double valor;
-
+	private String marca;
+	private String modelo;
+	private String anio;
+	private double valor;
+	
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	public String getAnio() {
+		return anio;
+	}
+	public void setAnio(String anio) {
+		this.anio = anio;
+	}
+	
+	public double getValor() {
+		return valor;
+	}
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
 }
--- src/com/krakedev/VehiculoTest.java
+++ src/com/krakedev/VehiculoTest.java
@@ -6,43 +6,42 @@ public class VehiculoTest {
 
 		Vehiculo v;
 		
-		v = new Vehiculo ();
+		v = new Vehiculo();
 
 		System.out.println("======= Vehículo 1 Creado ========");
 		
-		System.out.println("Año: " + v.anio);
-		System.out.println("Marca: " + v.marca);
-		System.out.println("Modelo: " + v.modelo);
+		System.out.println("Año: " + v.getAnio());
+		System.out.println("Marca: " + v.getMarca());
+		System.out.println("Modelo: " + v.getModelo());
 		
 		System.out.println("======= Vehículo 1 Valores Asignados ========");
 		
-		v.anio = "2012";
-		v.marca = "Chevrolet";
-		v.modelo = "nuevo";
+		v.setAnio("2012");
+		v.setMarca("Chevrolet");
+		v.setModelo("nuevo");
 		
-		System.out.println("Año: " + v.anio);
-		System.out.println("Marca: " + v.marca);
-		System.out.println("Modelo: " + v.modelo);
+		System.out.println("Año: " + v.getAnio());
+		System.out.println("Marca: " + v.getMarca());
+		System.out.println("Modelo: " + v.getModelo());
 		
 		System.out.println("======= Vehículo 2 Creado ========");
 
 		Vehiculo v2 = new Vehiculo();
 		
-		System.out.println("Año: " + v2.anio);
-		System.out.println("Marca: " + v2.marca);
-		System.out.println("Modelo: " + v2.modelo);
+		System.out.println("Año: " + v2.getAnio());
+		System.out.println("Marca: " + v2.getMarca());
+		System.out.println("Modelo: " + v2.getModelo());
 		
 		System.out.println("======= Vehículo 2 Valores Asignados ========");
 		
-		v2.anio = "2020";
-		v2.marca = "Toyota";
-		v2.modelo = "Runner";
+		v2.setAnio("2020");
+		v2.setMarca("Toyota");
+		v2.setModelo("Runner");
 		
-		System.out.println("Año: " + v2.anio);
-		System.out.println("Marca: " + v2.marca);
-		System.out.println("Modelo: " + v2.modelo);
+		System.out.println("Año: " + v2.getAnio());
+		System.out.println("Marca: " + v2.getMarca());
+		System.out.println("Modelo: " + v2.getModelo());
 				
-		
 	}
 
 }
